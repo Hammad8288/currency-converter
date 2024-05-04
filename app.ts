0796d372ca841ca0ef5e1200dadf8dc9e3802df4
Hammad8288 <159915404+Hammad8288@users.noreply.github.com>
2024-05-04 13:21:59 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const currency:any = {
+    USD : 1, // BASE CURRENCY
+    EUR : 0.91,
+    GBP : 0.76,
+    INR : 74.57,
+    PKR : 280 ,
+
+} ;
+
+let userAnswer = await inquirer.prompt([
+    {
+        name : "from" ,
+        type: "list" ,
+        message : "Enter From Currency",
+        choices :["USD", "EUR", "GBP", "INR", "PKR"] 
+
+    },
+    {
+        name : "to" ,
+        type: "list" ,
+        message : "Enter to Currency",
+        choices :["USD", "EUR", "GBP", "INR", "PKR"] 
+
+    },
+    {
+        name : "amount",
+        type: "input",
+        message: "Enter Your Amount",
+    }
+]) ;
+
+let fromAmount = currency[userAnswer.from]
+let toAmount = currency[userAnswer.to]
+let amount = userAnswer.amount
+let baseAmount = amount / fromAmount  // 200 / PKR 280 = 0.714
+let convertedAmount = Math.floor( baseAmount * toAmount)
+console.log(convertedAmount);
